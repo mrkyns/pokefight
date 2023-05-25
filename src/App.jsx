1fdd2aaadd130d0c969ea0845ec73f961bc81aad
Miroslav Ljubicic <127101426+mrkyns@users.noreply.github.com>
2023-05-25 23:24:51 +0200

    created navigation, added classes and created loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import Loader from "./components/Loader";
 import Logo from "./components/Logo";
+import Navigation from "./components/Navigation";
 
 function App() {
+  const dark = true;
+
   return (
-    <div className="bg-pattern bg-auto bg-repeat bg-black min-h-screen">
-      <Logo />
+    <div className="text-white box-border bg-pattern bg-auto bg-repeat bg-black min-h-screen flex flex-col items-center">
+      <Navigation />
+
+      <Loader />
+      {/* <Logo />
       <h1 className="font-pokefont text-6xl text-white">
         i gota fight them all !!!
-      </h1>
+      </h1>*/}
     </div>
   );
 }
